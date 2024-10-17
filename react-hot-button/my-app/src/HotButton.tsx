ed50eb51a58ff2b95e10aa6a83f0811a8104a9fd
Enrique Canel <enriquecanel1998@gmail.com>
2024-10-17 02:24:18 +0000

    completed react button

---
+++ react-hot-button/my-app/src/HotButton.tsx
@@ -0,0 +1,31 @@
+import './HotButton.css';
+
+export function HotButton() {
+  const [clicks, setClicks] = useState(0);
+  let colorIndex = Math.floor(clicks / 3);
+
+  console.log(colorIndex);
+  const colors: string[] = [
+    'purple',
+    'blue',
+    'red',
+    'green',
+    'yellow',
+    'white',
+  ];
+
+  if (colorIndex > colors.length) {
+    console.log(colors[colors.length - 1]);
+    colorIndex = colors.length - 1;
+  }
+  function handleClick() {
+    setClicks((prevClicks) => prevClicks + 1);
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className={`hot-button ${colors[colorIndex]}`}>
+      HOT BUTTON
+    </button>
+  );
+}
