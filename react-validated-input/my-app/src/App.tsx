f6885feeaff5198fd8bec45f4be16c48f6e6545b
Enrique Canel <enriquecanel1998@gmail.com>
2024-10-22 03:29:54 +0000

    Completed react validated input

---
+++ react-validated-input/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ValidatedInput } from './ValidatedInput';
+
+export function App() {
+  return (
+    <>
+      <ValidatedInput />
+    </>
+  );
+}
+
+export default App;
