fd5ce09fb4cbcc3c692d50f8029ebbe520cf9813
Enrique Canel <enriquecanel1998@gmail.com>
2024-11-15 01:58:16 +0000

    Completed react modal

--- react-modal/my-app/src/App.tsx
+++ react-modal/my-app/src/App.tsx
@@ -1,33 +1,38 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
 import './App.css';
+import { Modal } from './Modal';
 
-function App() {
-  const [count, setCount] = useState(0);
+export function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openDialog() {
+    setIsOpen(true);
+  }
+
+  function cancelClick() {
+    setIsOpen(false);
+  }
+
+  function deleteClick() {
+    alert('Something got deleted!');
+    cancelClick();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <button type="button" onClick={openDialog}>
+        Delete Me!
+      </button>
+      <Modal isOpen={isOpen} onClose={cancelClick}>
+        <button type="button" onClick={deleteClick}>
+          {' '}
+          Delete
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button type="button" onClick={cancelClick}>
+          {' '}
+          Cancel
+        </button>
+      </Modal>
     </>
   );
 }
--- react-modal/react-modal-quiz-notes.md
+++ react-modal/react-modal-quiz-notes.md
@@ -5,14 +5,15 @@
 Answer the following questions in the provided markdown file before turning in this exercise:
 
 - What is the `<dialog>` element used for?
-
+  A modal dialog box or another interactive component.
 - How do you show and hide a modal dialog?
-
+  Using the .show or .close method
 - How do you manipulate child components in React? Why will that no work for the `<dialog>` element?
-
+  You use useEffect. Because react components do not have functions you can call and you cannot use useRef to a custom component.
 - How do you call the dialog element's functions in React?
-
+  UseRef hook
 - How can you render nested components or JSX elements in React?
+  by creating child components
 
 ## Notes
 
